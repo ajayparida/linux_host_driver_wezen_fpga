7c53b2070fca7a083751fb197af909e579f8cb18
Ajay Parida <ajay.parida@nordicsemi.no>
2025-06-30 11:58:37 +0530

    TWT tear down depends upon flowid given by AP
    flow_id  check based upon the flow_id received in
    event NRF_WIFI_UMAC_EVENT_CONFIG_TWT (i.e. as received from AP).
    
    Signed-off-by: Ajay Parida <ajay.parida@nordicsemi.no>

--- nrf_wifi_driver.git/linux/fullmac/src/dbgfs_wlan_fmac_twt.c
+++ nrf_wifi_driver.git/linux/fullmac/src/dbgfs_wlan_fmac_twt.c
@@ -248,7 +248,7 @@ retry_twt:
 		tok_s = strstr(conf_buf, " flow_id=");
 		if (tok_s) {
 			param_get_val(tok_s, "flow_id=", &val);
-			if (val == rpu_ctx_lnx->twt_params.twt_cmd.twt_flow_id) {
+			if (val == rpu_ctx_lnx->twt_params.twt_event.twt_flow_id) {
 
 				twt_teardown_cmd = kzalloc(sizeof(*twt_teardown_cmd), GFP_KERNEL);
 				if (!twt_teardown_cmd) {
@@ -258,6 +258,7 @@ retry_twt:
 				twt_teardown_cmd->umac_hdr.cmd_evnt = NRF_WIFI_UMAC_CMD_TEARDOWN_TWT;
 				twt_teardown_cmd->umac_hdr.ids.wdev_id = 0;
 				twt_teardown_cmd->umac_hdr.ids.valid_fields |= NRF_WIFI_INDEX_IDS_WDEV_ID_VALID;
+				twt_teardown_cmd->info.twt_flow_id = val;
 
 				status = umac_cmd_cfg(fmac_ctx,
 						      twt_teardown_cmd,
